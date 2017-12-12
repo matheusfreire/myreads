6deb71292fef9d08ca6d6eee2538abe3cb25c748
Matheus Freire <matheus.sfreire@gmail.com>
2017-12-11 22:30:32 -0200

    Update listbooks with arrays

--- src/App.js
+++ src/App.js
@@ -7,20 +7,51 @@ import SearchBooks from './SearchBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    booksReading: [
+      {"id": "1",
+      "imageUrl": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+      "title": "To Kill a Mockingbird",
+      "author": "Harper Lee"
+      },
+      {"id": "2",
+      "imageUrl": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+      "title": "Ender`s Game",
+      "author": "Orson Scott Card"
+      }
+    ],
+    booksWant: [
+      {"id": "3",
+      "imageUrl": "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+      "title": "1776",
+      "author": "David McCullough"
+      },
+      {"id": "4",
+      "imageUrl": "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+      "title": "Harry Potter and the Sorcerers Stone",
+      "author": "J.K. Rowling"
+      }
+    ],
+    booksReaded: [
+      {"id": "5",
+      "imageUrl": "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
+      "title": "The hobbit",
+      "author": "J.R.R. Tolkien"
+      },
+      {"id": "6",
+      "imageUrl": "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
+      "title": "Oh, the Places You'll Go",
+      "author": "Seuss"
+      }
+    ]
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => 
-          <ListBooks />
+          <ListBooks booksReading={this.state.booksReading}
+          booksWant={this.state.booksWant}
+          booksReaded={this.state.booksReaded}/>
         }/>
         <Route exact path="/search" render={() =>
           <SearchBooks />
