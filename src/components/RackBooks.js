a3c143c827c990b8e6c9472a23ad6d555277abba
Matheus Freire <matheus.sfreire@gmail.com>
2017-12-20 22:26:50 -0200

    Change rackbook for stateless

--- src/components/RackBooks.js
+++ src/components/RackBooks.js
@@ -1,43 +1,35 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Loading from './Loading'
 import sortBy from 'sort-by'
 import Book from './Book'
 
-class RackBooks extends React.Component{
-    static propTypes = {
-        title:PropTypes.string.isRequired,
-        books:PropTypes.array.isRequired,
-        updateRack: PropTypes.func.isRequired
-    }
+const RackBooks = (props)=>{
 
-    render(){
-        const {title, books, showLoading, updateRack} = this.props
-        const bookSorted = books.sort(sortBy('name'))
-        return(
-            <div className="bookshelf">
-                <div className="bookshelf-title">
-                    <h2>{title}</h2>
-                </div>
-                { showLoading ? (
-                    <div className="center">
-                      <Loading />
-                    </div>
-                ): (
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {bookSorted.map(book => (
-                                <li key={book.id}>
-                                    {Book({book: book, rack: book.shelf, 
-                                        updateRack:(e) => {updateRack(e,book)}}
-                                    )}
-                                </li>
-                            ))}
-                        </ol>
-                    </div>
-                )}
+    const bookSorted = props.books.sort(sortBy('name'))
+    return(
+        
+        <div className="bookshelf">
+            <div className="bookshelf-title">
+                <h2>{props.title}</h2>
             </div>
-        )
-    }
+            { props.showLoading ? (
+                <div className="center">
+                    <Loading />
+                </div>
+            ): (
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {bookSorted.map(book => (
+                            <li key={book.id}>
+                                {Book({book: book, rack: book.shelf, 
+                                    updateRack:(e) => {props.updateRack(e,book)}}
+                                )}
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            )}
+        </div>
+    )
 }
 export default RackBooks
