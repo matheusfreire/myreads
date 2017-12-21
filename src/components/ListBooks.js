790e69540d2862bffdb34cabfa63b3d863137550
Matheus Freire <matheus.sfreire@gmail.com>
2017-12-20 22:32:30 -0200

    Change listbooks into stateless

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,50 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
 import RackBooks from './RackBooks'
 
-class ListBooks extends React.Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        updateRack:PropTypes.func.isRequired
-    }
-    state = {
-
-    }
-
-    isReading = (rack) =>{
+const ListBooks=(props) =>{
+    
+    function isReading(rack){
         return rack === "currentlyReading"
     }
 
-    isWanted = (rack) =>{
+    function isWanted(rack){
         return rack === "wantToRead"
     }
 
-    isReaded = (rack) =>{
+    function isReaded(rack){
         return rack === "read"
     }
 
-    render() {
-        const {showLoading, updateRack, books } = this.props
+    const booksReading = props.books.filter((book) => isReading(book.shelf))
+    const booksWant = props.books.filter((book) => isWanted(book.shelf))
+    const booksReaded= props.books.filter((book) => isReaded(book.shelf))
         
-        let booksReading = books.filter((book) => this.isReading(book.shelf))
-        let booksWant = books.filter((book) => this.isWanted(book.shelf))
-        let booksReaded= books.filter((book) => this.isReaded(book.shelf))
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <RackBooks title={'Currently Reading'} books={booksReading} showLoading={showLoading} updateRack={updateRack} />
-                    <RackBooks title={'Want to Read'} books={booksWant} showLoading={showLoading} updateRack={updateRack} />
-                    <RackBooks title={'Read'} books={booksReaded} showLoading={showLoading} updateRack={updateRack} />
-                </div>
-                <div className="open-search">
-                    <Link to='search'>Add a book</Link>
-                </div>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
             </div>
-        )
-    }
+            <div className="list-books-content">
+                {RackBooks(
+                    {title: 'Currently Reading', books: booksReading, 
+                    showLoading: props.showLoading, updateRack: props.updateRack}
+                )}
+                {RackBooks(
+                    {title: 'Want to Read', books: booksWant, 
+                    showLoading: props.showLoading, updateRack: props.updateRack}
+                )}
+                {RackBooks(
+                    {title: 'Read', books: booksReaded, 
+                    showLoading: props.showLoading, updateRack: props.updateRack}
+                )}
+            </div>
+            <div className="open-search">
+                <Link to='search'>Add a book</Link>
+            </div>
+        </div>
+    )
 }
 export default ListBooks
